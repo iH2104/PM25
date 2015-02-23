95429894c1b7124a53dad59797ce5edf01d2a5f0
jhands <ugot777@gmail.com>
2015-02-23 13:53:30 +0900

    Init

---
+++ pm25/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pm25
+//
+//  Created by JAY on 2015. 2. 23..
+//  Copyright (c) 2015년 jay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
