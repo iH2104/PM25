9b850859b2a01559e8d9b49c7cb299ee856d80a1
jhands <ugot777@gmail.com>
2015-02-24 14:49:02 +0900

    Update

--- pm25/ViewController.swift
+++ pm25/ViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        CLGeocoder().reverseGeocodeLocation(manager.location, completionHandler: { (placemarks, error) -> Void in
+            
+            if ( error != nil ) {
+                println("Error: " + error.localizedDescription)
+                return
+            }
+            
+            let pm = placemarks[0] as CLPlacemark
+            self.displayLocationInfo(pm)
+            
+        })
+    }
+    
+    @IBOutlet weak var locality: UILabel!
+    @IBOutlet weak var pm10Level: UILabel!
+    @IBOutlet weak var pm25Level: UILabel!
+    @IBOutlet weak var maskYn: UILabel!
+    @IBOutlet weak var bikeYn: UILabel!
+    
+    func displayLocationInfo(placemark: CLPlacemark) {
+        self.locationManager.stopUpdatingLocation()
+        
+        println(placemark.locality)
+        println(placemark.postalCode)
+        println(placemark.administrativeArea)
+        println(placemark.country)
+        
+        locality.text = placemark.locality
+        pm10Level.text = "PM10  30µ/m³"
+        pm25Level.text = "PM2.5 15µ/m³"
+        maskYn.text = "마스크 착용(X)"
+        bikeYn.text = "자전거 타기(O)"
+    }
 
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        println("Error: " + error.localizedDescription)
+    }
 }
-
